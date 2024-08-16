b54f05975569658e57e14e0cb7327cd6c99f4560
SaeedRz96 <saeedramezani75@gmail.com>
2024-08-16 16:21:00 +0330

    view filters added

--- bus_app/urls.py
+++ bus_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from bus_app.views import welcome
+from bus_app.views import welcome, ticket_list, ticket_list2
 
 
 urlpatterns = [
-    path('welcome', welcome)
+    path('welcome', welcome),
+    path('tickets', ticket_list),
+    path('tickets/<str:input_title>', ticket_list2)
 ] 
--- bus_app/views.py
+++ bus_app/views.py
@@ -1,5 +1,49 @@
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, JsonResponse
+from bus_app.models import Ticket
 
 
 def welcome(request):
     return HttpResponse("Welcome to Bus page")
+
+
+def ticket_list(request):
+    tickets = Ticket.objects.all()
+    my_ticket_list = []
+    for item in tickets:
+        ticket_dictionary = {
+            "title" : item.title,
+            "source" : item.source.name,
+            "destination" : item.destination.name
+        }
+        my_ticket_list.append(ticket_dictionary)
+    return JsonResponse(my_ticket_list, safe=False)
+
+
+def ticket_list2(request, input_title):
+    tickets = Ticket.objects.filter(title=input_title)
+    my_ticket_list = []
+    for item in tickets:
+        ticket_dictionary = {
+            "title" : item.title,
+            "source" : item.source.name,
+            "destination" : item.destination.name
+        }
+        my_ticket_list.append(ticket_dictionary)
+    return JsonResponse(my_ticket_list, safe=False)
+
+
+def ticket_detail(request, id):
+    ticket = Ticket.objects.get(id=id)
+
+
+def ticket_list3(request, input_source):
+    tickets = Ticket.objects.filter(source__name=input_source)
+    my_ticket_list = []
+    for item in tickets:
+        ticket_dictionary = {
+            "title" : item.title,
+            "source" : item.source.name,
+            "destination" : item.destination.name
+        }
+        my_ticket_list.append(ticket_dictionary)
+    return JsonResponse(my_ticket_list, safe=False)
