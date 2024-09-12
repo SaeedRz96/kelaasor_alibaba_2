897f84c97367a278dbf4ff145b9ba7effc903207
SaeedRz96 <saeedramezani75@gmail.com>
2024-09-12 10:54:38 +0330

    9

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import Login, Refresh,MySale, Register
+from .views import Login, Refresh,MySale, Register,GetOTP, OTPLogin
 
 
 urlpatterns = [
     path('login', Login.as_view()),
     path('refresh', Refresh.as_view()),
     path('my-sale', MySale.as_view()),
-    path('register', Register.as_view())
+    path('register', Register.as_view()),
+    path('get-otp', GetOTP.as_view()),
+    path('otp-login', OTPLogin.as_view())
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -6,10 +6,48 @@ from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from .serializers import UserRegisterSerializer
 from .models import Wallet
+from rest_framework.views import APIView
+import random
+from rest_framework.response import Response
+from django.core.cache import cache
+from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
 
 
 class Login(TokenObtainPairView):
     pass
+
+
+class GetOTP(APIView):
+    def post(self,request):
+        phone_number = request.data.get("phone")
+        otp = random.randint(1000,9999)
+        cache.set(phone_number,otp, timeout=180)
+        # SMS TO USER
+        return Response("Your Code for Is :{}".format(otp))
+
+
+class OTPLogin(APIView):
+    def post(self, request):
+        input_otp = request.data.get("input_otp")
+        phone_number = request.data.get("phone_number")
+        otp_valid = cache.get(phone_number)
+        if not otp_valid:
+            return Response("No otp!")
+        if otp_valid == input_otp:
+            cache.delete(phone_number)
+            user = User.objects.get(username=phone_number)
+            refresh_token = RefreshToken.for_user(user)
+            access_token = AccessToken.for_user(user)
+            return Response(
+                {
+                    "refresh_token" : str(refresh_token),
+                    "access_token" : str(access_token)
+                }
+            )
+            
+        else:
+            return Response("OTP is incorrect")
+        
 
 
 class Refresh(TokenRefreshView):
